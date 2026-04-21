0506f4c5d8ceccb8bc99f383186a088876691f21
Rene-Nun <christiannuntkd@hotmail.com>
2026-04-21 00:44:49 -0600

    Actualizar revender.js

--- api/revender.js
+++ api/revender.js
@@ -1,18 +1,18 @@
-import { Client } from “@notionhq/client”;
+import { Client } from "@notionhq/client";
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
 export default async function handler(req, res) {
-if (req.method !== “POST”) return res.status(405).end();
+if (req.method !== "POST") return res.status(405).end();
 
 const { id } = req.body;
-if (!id) return res.status(400).json({ error: “Falta id” });
+if (!id) return res.status(400).json({ error: "Falta id" });
 
 await notion.pages.update({
 page_id: id,
 properties: {
 Estado: {
-select: { name: “En Reventa” }
+select: { name: "En Reventa" }
 }
 }
 });
